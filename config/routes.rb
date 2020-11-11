ced9c340088b21b0c3482599a52f926b7b1c6dbb
shota-ino <73680080+shota-ino@users.noreply.github.com>
2020-11-11 18:28:30 +0900

    サーバーサイド実装
    URLをpathパラメーターでルーティング
    レスポンスの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   # get 'posts/new', to: 'posts#new'
   # ↑「投稿完了しました」というページは使用しないため
   post 'posts', to: 'posts#create'
+
+  # get 'posts', to: 'posts#checked'←queryパラメーターで設定した場合
+  # ↑必要なパラメーターは、「どのメモを既読したか」を判別するためのメモのid
+  # queryパラメーターを使用した場合、/posts/?id=1とリクエストを行うと、params[:id]にてパラメーターを取得することができる
+  get 'posts/:id', to: 'posts#checked'
+  # ↑今回のように渡す情報が一意の情報であればpathパラメーターの方が適している
+  # postのidであれば'posts/:id'のように記載するpathパラメーターの方が認識もしやすく、記述も単純
 end
